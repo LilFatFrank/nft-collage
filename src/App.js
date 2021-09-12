1cdf9761ba7ff78a292404a856a4f5933e888cee
LilFatFrank <karan.parsnani@ymail.com>
2021-09-12 21:24:25 +0530

    layout ready with dummy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.scss";
+import { dummyData } from "./data/dummyData";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${classes.app}`}>
+      <div className={`${classes.gridOne}`}>
+        <img src={`assets/images/rainbow-cat.gif`} alt={"rainbow-cat"} />
+        <h1 style={{ fontSize: "40px" }}>Flex and shill your NFTs</h1>
+        <button className={`${classes.button}`}>Connect Wallet</button>
+      </div>
+      <div className={`${classes.gridTwo}`}>
+        <div className={`${classes.header}`}>
+          <div className={`${classes.headerLabel}`}>
+            <div className={`${classes.catImage}`}>
+              <img src={`assets/images/header-cat.png`} alt={"header-cat"} />
+            </div>
+            <div style={{ fontSize: "24px" }}>YOUR NFT COLLECTION</div>
+          </div>
+          <div className={`${classes.siteName}`}>mynfts.show</div>
+        </div>
+        <div className={`${classes.content}`}>
+          {dummyData.rowOne.length ? (
+            <div className={`${classes.rowContainer}`}>
+              <div
+                className={`${classes.row} ${
+                  dummyData.rowOne.length > 4 ? classes.animate : ""
+                }`}
+              >
+                {dummyData.rowOne.map((image, index) => (
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className={`${classes.image}`}
+                    key={`${image.alt} - ${index}`}
+                  />
+                ))}
+              </div>
+            </div>
+          ) : null}
+          {dummyData.rowTwo.length ? (
+            <div className={`${classes.rowContainer}`}>
+              <div
+                className={`${classes.row} ${
+                  dummyData.rowTwo.length > 4 ? classes.reverse : ""
+                }`}
+              >
+                {dummyData.rowTwo.map((image, index) => (
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className={`${classes.image}`}
+                    key={`${image.alt} - ${index}`}
+                  />
+                ))}
+              </div>
+            </div>
+          ) : null}
+          {dummyData.rowThree.length > 0 ? (
+            <div className={`${classes.rowContainer}`}>
+              <div
+                className={` ${classes.row} ${
+                  dummyData.rowThree.length > 4 ? classes.animate : ""
+                }`}
+              >
+                {dummyData.rowThree.map((image, index) => (
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className={`${classes.image}`}
+                    key={`${image.alt} - ${index}`}
+                  />
+                ))}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data/dummyData.js
@@ -0,0 +1,104 @@
+export const dummyData = {
+  rowOne: [
+    {
+      src: `assets/images/squirrel.png`,
+      alt: `Squirrel`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/ArtGenesis.png`,
+      alt: `Art Genesis`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/punk.png`,
+      alt: `Punk`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/ape.png`,
+      alt: `Ape`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+  ],
+  rowTwo: [
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/punk.png`,
+      alt: `Punk`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/ape.png`,
+      alt: `Ape`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/loot.png`,
+      alt: `Loot`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/cat.png`,
+      alt: `Cat`,
+    },
+  ],
+  rowThree: [
+    {
+      src: `assets/images/loot.png`,
+      alt: `Loot`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/cat.png`,
+      alt: `Cat`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/squirrel.png`,
+      alt: `Squirrel`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+    {
+      src: `assets/images/ArtGenesis.png`,
+      alt: `Art Genesis`,
+    },
+    {
+      src: `assets/images/Question.png`,
+      alt: `Question`,
+    },
+  ],
+};
